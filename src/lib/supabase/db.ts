7bd89eb9827af52c5c75049ba135eb80b797742c
Roukaya Chelly <roukayachelly7@gmail.com>
2024-05-07 00:15:55 +0100

    update

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -7,18 +7,18 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
-  console.log("🔴 no database URL");
+  console.log(" no database URL");
 }
 
 const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
 const db = drizzle(client, { schema });
 const migrateDb = async () => {
   try {
-    console.log("🟠 Migrating client");
+    console.log(" Migrating client");
     await migrate(db, { migrationsFolder: "migrations" });
-    console.log("🟢 Successfully Migrated");
+    console.log(" Successfully Migrated");
   } catch (error) {
-    console.log("🔴 Error Migrating client", error);
+    console.log(" Error Migrating client", error);
   }
 };
 migrateDb();
